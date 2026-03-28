3f9e531da68e8d650aff1b04f182ce8171a392ef
jermorg <sashashulga7775@gmail.com>
2026-03-28 11:01:09 +0000

    add enable or disable futer in .env | Added the ability to work with TikTok via Cobalt running on server

--- index.js
+++ index.js
@@ -15,25 +15,81 @@ const TEXT_FOR_NO_OPTIMAL_FILE = process.env.TEXT_FOR_NO_OPTIMAL_FILE;
 // Events
 telegramBot.on('message', async (msg) => {
     const chatId = msg.chat.id;
-  
-    // prepare sender 
     const webhookOptions = {
         username: msg.from.first_name || 'user',
         avatarURL: await getUserPhotoUrl(chatId) || undefined,
     };
-
-    if(msg.photo || msg.video || msg.document || msg.video_note){
+    if(
+        (process.env.MESSAGE_PHOTO === "true" && msg.photo) ||
+        (process.env.MESSAGE_VIDEO === "true" && msg.video) ||
+        (process.env.MESSAGE_GIF === "true" && msg.document) ||
+        (process.env.MESSAGE_VIDEO_NOTE === "true" && msg.video_note)
+    ){
         const bestMedia = await getTelegramMediaUrl(msg);
         if(bestMedia.ok){
             await discordWebhook.send({
-                files: [bestMedia.url],
+                files: [{
+                    attachment: bestMedia.url
+                }],
                 ...webhookOptions,
             });
+            return telegramBot.sendMessage(chatId, '✅');
+        } else { return telegramBot.sendMessage(msg.chat.id, bestMedia.msg) }
+    }
+
+    if (
+        process.env.TIKTOK === "true" && (
+            msg.text?.startsWith('https://www.tiktok.com/') ||
+            msg.text?.startsWith('https://vt.tiktok.com/')
+    )) {
+        const filesToSend = await downloadTikTokVideo(msg.text); 
+        if (filesToSend && filesToSend.length > 0) {
+            await discordWebhook.send({
+                files: filesToSend,
+                ...webhookOptions,
+            });
+            return telegramBot.sendMessage(chatId, '✅');
         } else {
-            telegramBot.sendMessage(msg.chat.id, bestMedia.msg)
+            return telegramBot.sendMessage(chatId, '❌');
         }
     }
+
+    if(process.env.MESSAGE_TEXT === "true" && msg.text){
+        await discordWebhook.send({ content: msg.text, ...webhookOptions });
+        return telegramBot.sendMessage(chatId, '✅');
+    }
 })
+
+// TikTok download function
+async function downloadTikTokVideo(url) {
+    const response = await axios.post(process.env.COBALT_LOCAL_URL, { localProcessing: "preferred", url: url }, {headers: { "Accept": "application/json", "Content-Type": "application/json" }});
+    const data = response.data;
+    if (!["redirect", "tunnel", "picker"].includes(data.status)) return null;
+    if (data.status === "redirect" || data.status === "tunnel") {
+        try {
+            const videoBuffer = await downloadAsBuffer(data.url);
+            return [{ attachment: videoBuffer, name: data.filename || 'video.mp4' }];
+        } catch (e) { console.error("❌ error download video to buffer:", e.message); return null; }
+    } else if (data.status === "picker") {
+        const files = [];
+        if (data.audio) {
+            files.push({ 
+                attachment: data.audio, 
+                name: 'music_from_video.mp3' 
+            });
+        }
+        if (Array.isArray(data.picker)) {
+            data.picker.filter(item => item.type === 'photo').forEach((item, index) => {
+                files.push({
+                    attachment: item.url,
+                    name: `photo_${Date.now()}_${index}.jpeg`
+                });
+            });
+        }   
+        return files.length > 0 ? files : null;
+    }
+    return null;
+}
 
 
 // Functions
@@ -65,9 +121,13 @@ function canSendTelegramMedia(_bite){
     return MAX_FILE_SIZE_IN_BITE > _bite;
 }
 
-
 function getBestMedia(Array) {
     return Array
         .filter(p => p.file_size <= MAX_FILE_SIZE_IN_BITE)
         .sort((a, b) => b.file_size - a.file_size)[0]; 
+}
+
+async function downloadAsBuffer(url) {
+    const response = await axios.get(url, { responseType: 'arraybuffer' });
+    return Buffer.from(response.data);
 }
